37a7f04dfa3e2b503f3f42ee4ea70cbe0b4c0a79
Your Name <>
2024-05-26 23:11:32 +0530

    new config added by pj

--- ec2-webserver/backend.tf
+++ ec2-webserver/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-remotestate-7856"
     key            = "terraform.tfstate"
-    encrypt        = true
     region         = "ap-south-1"
   }
 }
