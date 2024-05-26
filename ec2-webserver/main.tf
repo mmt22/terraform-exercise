a8790194a9010517482745bcaf21deabca7bb626
Your Name <>
2024-05-26 23:33:09 +0530

    new config added by pj

--- ec2-webserver/main.tf
+++ ec2-webserver/main.tf
@@ -25,6 +25,33 @@ resource "aws_instance" "apache2-linux" {
 
 }
 
+resource "aws_instance" "apache2-linux" {
+
+  ami                    = "ami-0f58b397bc5c1f2e8"
+  instance_type          = "t3.micro"
+  key_name               = "website"
+  vpc_security_group_ids = ["sg-06dfa5c2367f1db33"]
+  iam_instance_profile   = "SSM-ROLE-EC2"
+  availability_zone      = "ap-south-1a"
+  user_data              = file("./apache2.sh")
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 8
+    delete_on_termination = true
+    encrypted             = true
+    tags = {
+      "lifecyclepolicy" : "yes"
+    }
+  }
+  tags = {
+
+    "Name" : "apache2-linux-2"
+    "env" : "dev"
+    "os" : "ubuntu"
+  }
+
+}
+
 ### Security Group Rules ####
 
 
