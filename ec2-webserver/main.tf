7d66f91147f923124cab3368ad48eeb600da197d
Your Name <>
2024-05-26 23:33:59 +0530

    new config added by pj

--- ec2-webserver/main.tf
+++ ec2-webserver/main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "apache2-linux" {
 
 }
 
-resource "aws_instance" "apache2-linux" {
+resource "aws_instance" "apache2-linux-2" {
 
   ami                    = "ami-0f58b397bc5c1f2e8"
   instance_type          = "t3.micro"
