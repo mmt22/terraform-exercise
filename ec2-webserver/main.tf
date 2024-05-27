b5baa2bc74ffbfd38c10346c8c86436a9c983ebf
Your Name <>
2024-05-27 09:15:30 +0530

    new file

--- ec2-webserver/main.tf
+++ ec2-webserver/main.tf
@@ -24,32 +24,7 @@ resource "aws_instance" "apache2-ec2" {
 
 }
 
-resource "aws_instance" "nginx-ec2" {
 
-  ami                    = "ami-0f58b397bc5c1f2e8"
-  instance_type          = "t3.micro"
-  key_name               = "website"
-  vpc_security_group_ids = ["sg-06dfa5c2367f1db33"]
-  iam_instance_profile   = "SSM-ROLE-EC2"
-  availability_zone      = "ap-south-1a"
-  user_data              = file("./nginx.sh")
-  root_block_device {
-    volume_type           = "gp3"
-    volume_size           = 8
-    delete_on_termination = true
-    encrypted             = true
-    tags = {
-      "lifecyclepolicy" : "yes"
-    }
-  }
-  tags = {
-
-    "Name" : "nginx-webapp-2"
-    "env" : "dev"
-    "os" : "ubuntu"
-  }
-
-}
 
 ### Security Group Rules whitelist ####
 
