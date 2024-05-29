80c6805da2b4588a0754cc8a4dddb3caabd23e77
Your Name <you@example.com>
2024-05-29 18:39:50 +0530

    new project pratik joshi

--- 3-tier-web/backend.tf
+++ 3-tier-web/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-remotestate-7856"
-    key    = "/3-tier-web/terraform.tfstate"
+    key    = "3-tier-web/terraform.tfstate"
     region = "ap-south-1"
   }
 }
